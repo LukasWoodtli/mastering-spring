1680c61e3e6726fbe088bdd67e52f318335ba8ea
Lukas Woodtli <woodtli.lukas@gmail.com>
2021-10-12 16:03:53 +0200

    Add API documentation
    swagger-ui is not working as expected.

---
+++ ch06_todo/src/main/java/com/woodtli/todo/SwaggerConfig.java
@@ -0,0 +1,23 @@
+package com.woodtli.todo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+@EnableOpenApi
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        // get the API doc on http://localhost:8080/v3/api-docs
+        return new Docket(DocumentationType.OAS_30)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}

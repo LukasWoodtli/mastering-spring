a0debf27177a3434591275c07231803150f3f2b8
Lukas Woodtli <woodtli.lukas@gmail.com>
2021-11-29 15:17:10 +0100

    Add services for cloud example
    Change-Id: Ic63e4b279a235edb3f5317918ed8f3a560ed3247

---
+++ ch12_cloud/microservice-a/src/main/java/com/woodtli/mastering/spring/microservicea/contoller/MessageController.java
@@ -0,0 +1,25 @@
+package com.woodtli.mastering.spring.microservicea.contoller;
+
+import com.woodtli.mastering.spring.microservicea.beans.ApplicationConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class MessageController {
+
+    @Autowired
+    private ApplicationConfiguration configuration;
+
+    @RequestMapping("/message")
+    public Map<String, String> welcome() {
+
+        Map<String, String> map = new HashMap<>();
+        map.put("message", configuration.getMessage());
+
+        return map;
+    }
+}
